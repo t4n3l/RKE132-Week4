ccb259b8bad45e7d37003afdf0b4f30240121ee8
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-18 20:33:29 +0200

    moodletest

---
+++ MoodleTest/Program.cs
@@ -0,0 +1,51 @@
+﻿//Random rnd = new Random();
+//int cpuRnd, userRnd;
+//int cpuScore = 0;
+//int userScore = 0;
+//for (int i = 0; i < 3; i++)
+//{
+//    cpuRnd = rnd.Next(1, 7);
+//    userRnd = rnd.Next(1, 7);
+//    if (cpuRnd > userRnd)
+//    {
+//        cpuScore++;
+//        Console.WriteLine(cpuScore);
+//    }
+//    else if (cpuRnd < userRnd)
+//    {
+//        userScore++;
+//        Console.WriteLine(userScore);
+//    }
+//    else
+//    {
+//        continue; //programm lihtsalt jätkab oma tööd
+//    }
+//}
+
+//Random rnd = new Random();
+//int cpuRnd, userRnd;
+//int cpuScore = 0;
+//int userScore = 0;
+//cpuRnd = rnd.Next(1, 7);
+//userRnd = rnd.Next(1, 7);
+//Console.WriteLine(cpuRnd);
+//Console.WriteLine(userRnd);
+
+//if (cpuRnd > userRnd)
+//{
+//    cpuScore++;
+//}
+//else if (cpuRnd < userRnd)
+//{
+//    userScore++;
+//}
+
+for (int i = 3; i > 0; i--)
+{
+    Console.WriteLine("Hello, World!");
+}
+
+for (int i = 1; i < 5; i++)
+{
+    Console.WriteLine("Don't worry, be happy :)");
+}
