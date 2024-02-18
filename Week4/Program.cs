ce40e2f736dd5f73236bf9142bfb7f2da0d2f6c9
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-18 20:18:33 +0200

    Nädal 4

--- Week4/Program.cs
+++ Week4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i = 0;
+
+for (i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education");
+}
+Console.WriteLine("Have a nice day!");
+Console.WriteLine(i);
